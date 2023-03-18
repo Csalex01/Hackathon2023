04b99ad3a7561892bc6a384a9996d79fd71b2c15
Csalex01 <csillag.alex@student.ms.sapientia.ro>
2023-03-18 13:42:32 +0200

    Changed layout

--- FlatRoute/static/scripts/map.js
+++ FlatRoute/static/scripts/map.js
@@ -1,13 +1,27 @@
 import Map from "./ol/Map.js"
-import View from './ol/View.js';
-import OSM from './ol/source/OSM.js';
-import TileLayer from './ol/layer/Tile.js';
+import View from './ol/View.js'
+import OSM from './ol/source/OSM.js'
+import TileLayer from './ol/layer/Tile.js'
+import { fromLonLat } from './ol/proj.js'
+
+navigator.geolocation.getCurrentPosition(pos => {
+    let coords = {
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude
+    }
+
+    sessionStorage.setItem("current_location", JSON.stringify(coords))
+})
+
+const loc = JSON.parse(sessionStorage.getItem("current_location"))
+
+console.log(loc)
 
 const source = new OSM();
 const layer = new TileLayer({ source: source });
 const view = new View({
-    center: [0, 0],
-    zoom: 2
+    center: fromLonLat([loc.lon, loc.lat]),
+    zoom: 15
 })
 
 const map = new Map({
