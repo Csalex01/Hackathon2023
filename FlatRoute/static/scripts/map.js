6d7b1cb2be51bf016438976d081a0838c52857f5
Csalex01 <csillag.alex@student.ms.sapientia.ro>
2023-03-18 16:08:33 +0200

    Added markers

--- FlatRoute/static/scripts/map.js
+++ FlatRoute/static/scripts/map.js
@@ -1,31 +1,52 @@
-import Map from "./ol/Map.js"
-import View from './ol/View.js'
-import OSM from './ol/source/OSM.js'
-import TileLayer from './ol/layer/Tile.js'
-import { fromLonLat } from './ol/proj.js'
+import Map from 'https://cdn.skypack.dev/ol/Map.js';
+import View from 'https://cdn.skypack.dev/ol/View.js'
+import OSM from 'https://cdn.skypack.dev/ol/source/OSM.js'
+import TileLayer from 'https://cdn.skypack.dev/ol/layer/Tile.js'
 
-navigator.geolocation.getCurrentPosition(pos => {
-    let coords = {
-        lat: pos.coords.latitude,
-        lon: pos.coords.longitude
-    }
+import VectorLayer from 'https://cdn.skypack.dev/ol/layer/Vector.js';
+import VectorSource from 'https://cdn.skypack.dev/ol/source/Vector.js';
 
-    sessionStorage.setItem("current_location", JSON.stringify(coords))
-})
+import { Style, Icon } from "https://cdn.skypack.dev/ol/style"
+
+import { Feature } from "https://cdn.skypack.dev/ol"
+import { Point } from "https://cdn.skypack.dev/ol/geom"
+
+import { fromLonLat, transform } from 'https://cdn.skypack.dev/ol/proj'
+import { Control, defaults as defaultControls } from "https://cdn.skypack.dev/ol/control"
+
+import "./utils.js"
+import { HomeButtonControl, mousePositionControl, epsg3857toEpsg4326 } from "./utils.js"
 
 const loc = JSON.parse(sessionStorage.getItem("current_location"))
 
-console.log(loc)
-
-const source = new OSM();
-const layer = new TileLayer({ source: source });
-const view = new View({
-    center: fromLonLat([loc.lon, loc.lat]),
-    zoom: 15
-})
+let markers = new VectorLayer({
+    source: new VectorSource(),
+    style: new Style({
+        image: new Icon({
+            anchor: [0.5, 1],
+            src: 'https://icons.iconarchive.com/icons/paomedia/small-n-flat/64/map-marker-icon.png'
+        })
+    })
+});
 
 const map = new Map({
-    layers: [layer],
-    view: view,
-    target: 'map',
+    controls: defaultControls().extend([
+        new HomeButtonControl(),
+        mousePositionControl
+    ]),
+    layers: [
+        new TileLayer({ source: new OSM() }),
+        markers
+    ],
+    view: new View({
+        center: fromLonLat([loc.lon, loc.lat]),
+        zoom: 15
+    }),
+    target: 'map'
 });
+
+map.on("click", (evt) => {
+    let clicked_pos = epsg3857toEpsg4326(evt.coordinate)
+    console.log(clicked_pos)
+    markers.getSource().addFeature(new Feature(new Point(fromLonLat([clicked_pos[0], clicked_pos[1]]))));
+})
---
+++ FlatRoute/static/scripts/utils.js
@@ -0,0 +1,60 @@
+import { Control, defaults as defaultControls } from "./ol/control.js"
+import MousePosition from './ol/control/MousePosition.js';
+import { createStringXY } from './ol/coordinate.js';
+import { fromLonLat } from './ol/proj.js'
+
+class HomeButtonControl extends Control {
+    constructor(opt_options) {
+        const options = opt_options || {}
+
+        const button = document.createElement('button');
+        button.innerHTML = 'H';
+
+        const element = document.createElement('div');
+        element.className = 'home-button rotate-north ol-unselectable ol-control';
+        element.appendChild(button);
+
+        super({
+            element: element,
+            target: options.target
+        })
+
+        button.addEventListener('click', this.handle.bind(this), false);
+    }
+
+    handle() {
+        let loc = JSON.parse(sessionStorage.getItem("current_location"))
+
+        this.getMap().getView().setCenter(fromLonLat([loc.lon, loc.lat]))
+        this.getMap().getView().setZoom(15)
+    }
+}
+
+const mousePositionControl = new MousePosition({
+    coordinateFormat: createStringXY(4),
+    projection: 'EPSG:4326',
+});
+
+const epsg3857toEpsg4326 = (pos) => {
+    let x = pos[0];
+    let y = pos[1];
+    x = (x * 180) / 20037508.34;
+    y = (y * 180) / 20037508.34;
+    y = (Math.atan(Math.pow(Math.E, y * (Math.PI / 180))) * 360) / Math.PI - 90;
+    return [x, y];
+}
+
+navigator.geolocation.getCurrentPosition(pos => {
+    let coords = {
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude
+    }
+
+    sessionStorage.setItem("current_location", JSON.stringify(coords))
+}, (error) => alert(error), { enableHighAccuracy: true })
+
+export {
+    HomeButtonControl,
+    mousePositionControl,
+    epsg3857toEpsg4326
+}
