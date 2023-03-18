2ad1d45cd5ac639642d11de21c309504769b2534
Csalex01 <csillag.alex@student.ms.sapientia.ro>
2023-03-18 12:26:06 +0200

    Added OpenLayers

---
+++ FlatRoute/static/map.js
@@ -0,0 +1,13 @@
+
+const map = new Map({
+    target: 'map',
+    layers: [
+        new TileLayer({
+            source: new OSM(),
+        }),
+    ],
+    view: new View({
+        center: [0, 0],
+        zoom: 2,
+    }),
+});
